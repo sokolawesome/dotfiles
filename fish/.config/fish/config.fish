7ac7e6723358bae5e89a1d8991c5d5abbf0b13c0
sokolawesome <sokolawesome@gmail.com>
2025-06-15 22:06:28 +0300

    feat: more configs, styling

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,13 @@
-set -g fish_greeting
-
-zoxide init fish | source
+if status is-login
+    if test (tty) = "/dev/tty1"
+        exec dbus-run-session Hyprland
+    end
+end
 
 if test -f ~/.cache/wal/colors.fish
     source ~/.cache/wal/colors.fish
 end
+
+set -g fish_greeting
+
+zoxide init fish | source
---
+++ hypr/.config/hypr/scripts/xdg.sh
@@ -0,0 +1,38 @@
+_sleep1="0.1"
+_sleep2="0.5"
+_sleep3="2"
+_sleep4="1"
+
+sleep $_sleep2
+
+killall -e xdg-desktop-portal-hyprland
+killall -e xdg-desktop-portal-gtk
+killall -e xdg-desktop-portal
+
+dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=hyprland
+systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
+systemctl --user start graphical-session.target
+
+systemctl --user stop pipewire
+systemctl --user stop wireplumber
+systemctl --user stop xdg-desktop-portal
+systemctl --user stop xdg-desktop-portal-gtk
+systemctl --user stop xdg-desktop-portal-hyprland
+sleep $_sleep1
+
+/usr/lib/xdg-desktop-portal-hyprland &
+sleep $_sleep3
+
+/usr/lib/xdg-desktop-portal-gtk
+sleep $_sleep1
+
+/usr/lib/xdg-desktop-portal &
+sleep $_sleep2
+
+systemctl --user start pipewire
+systemctl --user start wireplumber
+systemctl --user start xdg-desktop-portal
+systemctl --user start xdg-desktop-portal-gtk
+systemctl --user start xdg-desktop-portal-hyprland
+
+sleep $_sleep4
