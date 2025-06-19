8d038724a7049e00fd828388cca3783b653e00a7
sokolawesome <sokolawesome@gmail.com>
2025-06-19 04:25:44 +0300

    feat: some changes
    - media script with string manipulations
    - waybar module fix

--- config/waybar/scripts/media.sh
+++ config/waybar/scripts/media.sh
@@ -2,12 +2,17 @@
 
 player_status=$(playerctl status 2>/dev/null)
 
-if [ "$player_status" = "Playing" ] || [ "$player_status" = "Paused" ]; then
-    artist=$(playerctl metadata artist 2>/dev/null)
-    title=$(playerctl metadata title 2>/dev/null)
-    if [ -n "$artist" ] && [ -n "$title" ]; then
+if [[ "$player_status" == "Playing" || "$player_status" == "Paused" ]]; then
+    clean_text() {
+        grep -oP '[\p{L}\p{N} -]+' | tr -d '\n' | sed -E 's/ +/ /g; s/^\s+|\s+$//g'
+    }
+
+    artist=$(playerctl metadata artist 2>/dev/null | clean_text)
+    title=$(playerctl metadata title 2>/dev/null | clean_text)
+
+    if [[ -n "$artist" && -n "$title" ]]; then
         echo "$artist - $title"
-    elif [ -n "$title" ]; then
+    elif [[ -n "$title" ]]; then
         echo "$title"
     else
         echo "Playing media"
